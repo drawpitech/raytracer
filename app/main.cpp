6cbd100f8d1541e33cf7a3a284c34dec8c3f3fdd
Baptiste Girardeau <contact@bgirardeau.me>
2024-04-16 11:11:28 +0200

    chore: init repository

---
+++ app/main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** raytracer
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
